12a147d85f6a7ff6071f67b2cabd9b91cdb47601
Daniel Winsor <danielkwinsor@gmail.com>
2016-04-26 18:25:21 -0700

    V0.3.3 Creating BigInteger scalar, because it is not well serialized.  Should probably do the same for BigDecimal, etc.

--- src/main/java/graphql/java/generator/Scalars.java
+++ src/main/java/graphql/java/generator/Scalars.java
@@ -1,8 +1,46 @@
 package graphql.java.generator;
 
+import java.math.BigInteger;
+
+import graphql.language.IntValue;
+import graphql.language.StringValue;
+import graphql.schema.Coercing;
 import graphql.schema.GraphQLScalarType;
 
 public class Scalars {
+    
+    public static GraphQLScalarType GraphQLBigInteger = new GraphQLScalarType("BigInteger", "Built-in java.math.BigInteger", new Coercing() {
+        @Override
+        public Object serialize(Object input) {
+            if (input instanceof BigInteger) {
+                return (BigInteger) input;
+            } else if (input instanceof String) {
+                return new BigInteger((String) input);
+            } else if (input instanceof Integer) {
+                return BigInteger.valueOf((Integer) input);
+            } else if (input instanceof Long) {
+                return BigInteger.valueOf((Long) input);
+            } else {
+                return null;
+            }
+        }
+
+        @Override
+        public Object parseValue(Object input) {
+            return serialize(input);
+        }
+
+        @Override
+        public Object parseLiteral(Object input) {
+            if (input instanceof StringValue) {
+                return BigInteger.valueOf(Long.parseLong(((StringValue) input).getValue()));
+            } else if (input instanceof IntValue) {
+                return BigInteger.valueOf(((IntValue) input).getValue());
+            }
+            return null;
+        }
+    });
+
     /**
      * Returns null if not a scalar/primitive type
      * Otherwise returns an instance of GraphQLScalarType from Scalars.
@@ -25,6 +63,9 @@ public class Scalars {
         }
         if (Boolean.class.isAssignableFrom(clazz) || boolean.class.isAssignableFrom(clazz)) {
             return graphql.Scalars.GraphQLBoolean;
+        }
+        if (BigInteger.class.isAssignableFrom(clazz)) {
+            return GraphQLBigInteger;
         }
         return null;
     }
