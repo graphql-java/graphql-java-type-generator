1314ebc82ee206703a73275af181c042524b3c66
Daniel Winsor <danielkwinsor@gmail.com>
2016-04-30 20:31:58 -0700

    V0.3.11 TypeResolverStrategy_Caching now caches.

--- src/main/java/graphql/java/generator/type/resolver/TypeResolverStrategy_Caching.java
+++ src/main/java/graphql/java/generator/type/resolver/TypeResolverStrategy_Caching.java
@@ -6,16 +6,22 @@ import graphql.java.generator.type.resolver.TypeResolver_BuildContextAware;
 import graphql.schema.TypeResolver;
 
 /**
- * TODO Caching.
+ * Reuses 1 {@link TypeResolver_BuildContextAware} per {@link BuildContext}
+ * since this TypeResolverStrategy_Caching itself is tied to only 1 BuildContext.
  * @author dwinsor
  *
  */
 public class TypeResolverStrategy_Caching
         extends UnsharableBuildContextStorer
         implements TypeResolverStrategy {
+    
+    TypeResolver_BuildContextAware typeResolver = null;
 
     @Override
     public TypeResolver getTypeResolver(Object object) {
-        return new TypeResolver_BuildContextAware(getContext());
+        if (typeResolver == null) {
+            typeResolver = new TypeResolver_BuildContextAware(getContext());
+        }
+        return typeResolver;
     }
 }
